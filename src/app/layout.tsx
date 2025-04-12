4f87396794fa163bf8cf3949f2358f1595d68e06
BillyXie23 <330764937@qq.com>
2025-04-12 09:33:13 +0800

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9VR4GTH04K"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9VR4GTH04K');
+          `
+        }} />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
